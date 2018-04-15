b9b049f42a62a381cdb0a5b756b44931fa5e89f5
Vivek Mani <22831490+vivekmani28@users.noreply.github.com>
2018-04-15 13:10:27 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-grep -A 1 itrust /home/ubuntu/DevOps_Project/hosts | grep -v itrust | awk '{print $1}' > /tmp/hosts_ip
+grep -A 1 itrust /home/ubuntu/DevOps_Project/hosts | awk '{print $1}' |  awk 'NR%2{printf "%s ",$0;next;}1' > /tmp/hosts_ip
 i=1;
 cat /tmp/hosts_ip | while read line;
 do
-count=`ssh -n -i ~/DevOps_Project/ec2.key $line "ps -ef | grep jetty | grep -v grep | wc -l"`
-echo "iTrust2 Instance #${i}, ${count}"
+ip=`echo $line |  awk '{print $2}'`
+count=`ssh -n -i ~/DevOps_Project/ec2.key $ip "ps -ef | grep jetty | grep -v grep | wc -l"`
+echo "iTrust2 Instance #${i} - ${ip}, ${count}"
 i=$((i+1))
 done
