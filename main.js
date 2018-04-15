63b5f97db2587f21ce50ebec753d24043623414a
Vivek Mani <22831490+vivekmani28@users.noreply.github.com>
2018-04-15 14:18:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,28 +6,28 @@ var exec = require('child_process').exec;
 var io = require('socket.io')(3000);
 
 io.on('connection', function (socket) {
-	var heartbeatTimer = setInterval(function () {
+        var heartbeatTimer = setInterval(function () {
 
-		exec(". ./script.sh", (err, stdout, stderr) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			var lines = stdout.toString().split('\n');
-			lines.forEach(function (line) {
-				if (line.length > 0) {
-					console.log(line);
-					var values = line.toString().split(',');
-					var obj = { "name": values[0], "count": values[1] };
-					socket.emit("heartbeat", obj);
-				}
-			});
-		});
-	}, 5000);
+                exec(". ./script.sh", (err, stdout, stderr) => {
+                        if (err) {
+                                console.log(err);
+                                return;
+                        }
+                        var lines = stdout.toString().split('\n');
+                        lines.forEach(function (line) {
+                                if (line.length > 0) {
+                                        var values = line.toString().split(',');
+                                        var temp = values[0].toString().split('-');
+                                        var obj = { "name": temp[0],"ip": temp[1], "count": values[1] };
+                                        console.log(obj);
+                                        socket.emit("heartbeat", obj);
+                                }
+                        });
+                });
+        }, 5000);
 
-	socket.on('disconnect', function () {
-		console.log("closing connection")
-		clearInterval(heartbeatTimer);
-	});
+        socket.on('disconnect', function () {
+                console.log("closing connection")
+                clearInterval(heartbeatTimer);
+        });
 });
-
